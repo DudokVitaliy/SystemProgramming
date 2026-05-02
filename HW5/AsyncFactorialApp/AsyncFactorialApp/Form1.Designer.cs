3b118e3e0dbdbffded5fdb4b8fe244eaedd4af85
Dudok_kh03@student.itstep.org <Dudok_kh03@student.itstep.org>
2026-05-02 21:44:54 +0300

    HW5

---
+++ HW5/AsyncFactorialApp/AsyncFactorialApp/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace AsyncFactorialApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.ListBox listBoxResults;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.listBoxResults = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+
+            this.textBoxNumber.Location = new System.Drawing.Point(20, 20);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(150, 20);
+            this.textBoxNumber.TabIndex = 0;
+
+            this.buttonCalculate.Location = new System.Drawing.Point(200, 18);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(120, 23);
+            this.buttonCalculate.TabIndex = 1;
+            this.buttonCalculate.Text = "Обчислити";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+
+            this.listBoxResults.FormattingEnabled = true;
+            this.listBoxResults.Location = new System.Drawing.Point(20, 60);
+            this.listBoxResults.Name = "listBoxResults";
+            this.listBoxResults.Size = new System.Drawing.Size(300, 200);
+            this.listBoxResults.TabIndex = 2;
+
+            this.ClientSize = new System.Drawing.Size(400, 300);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.listBoxResults);
+            this.Name = "Form1";
+            this.Text = "Факторіал (Async/Await)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ HW5/AsyncFactorialApp/AsyncFactorialApp/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncFactorialApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBoxNumber.Text, out int number))
+            {
+                buttonCalculate.Enabled = false;
+
+                long result = await CalculateFactorialAsync(number);
+
+                listBoxResults.Items.Add($"{number}! = {result}");
+
+                buttonCalculate.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Введіть коректне число!");
+            }
+        }
+
+        private async Task<long> CalculateFactorialAsync(int n)
+        {
+            return await Task.Run(() =>
+            {
+                long result = 1;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    result *= i;
+
+                    Thread.Sleep(200);
+                }
+
+                return result;
+            });
+        }
+    }
+}
